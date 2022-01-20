4f5e26796bac70d0182460591011213a583fbf7d
grigormartirosyan <grigormartirosyan700@gmail.com>
2022-01-20 17:03:12 +0400

    starting a project

---
+++ src/main/java/com/example/healthcare_recorder/HealthcareRecorderApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcare_recorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareRecorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareRecorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/healthcare_recorder/HealthcareRecorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.healthcare_recorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareRecorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
